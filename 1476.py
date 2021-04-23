7f5a92cc3db8c8c03d88616d617e9f8c1156fdc3
Jinyoung <roghabo@gmail.com>
2021-04-23 20:05:16 +0900

    1476

---
+++ 1476.py
@@ -0,0 +1,25 @@
+e, s, m = map(int, input().split())
+result = 0
+i = 0
+j = 0
+k = 0
+while True:
+    if(i == 15):
+        i = 1
+    else:
+        i += 1
+
+    if(j == 28):
+        j = 1
+    else:
+        j += 1
+
+    if(k == 19):
+        k = 1
+    else:
+        k += 1
+    
+    result +=1
+    if(i==e and j==s and k==m):
+        print(result)
+        break
